42226457add82a59da5aada795f6c10439ed143d
David Chase <drchase@google.com>
2019-05-30 15:19:26 -0400

    Copied repo over wholesale and tossed all its history because git is awful.

---
+++ experiment/run_trials.bash
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+if [ "z${MAXNOISE}" = "z" ] ; then
+    MAXNOISE=1
+    echo Lacking environment variable MAXNOISE, using default value "${MAXNOISE}" (percent)
+fi
+
+if [ "z${BENCH}" = "z" ] ; then
+    BENCH="semver"
+    echo Lacking environment variable BENCH, using default value "${BENCH}"
+fi
+
+# Perflock is preferred
+PERFLOCK=`which perflock`
+if [ "z${PERFLOCK}" = "z" -a `uname` = "Linux" ] ; then
+    echo "You can get cleaner benchmark results on Linux with perflock: go get github.com/aclements/perflock/..."
+fi
+
+if [ "z"`which bent` = "z" ] ; then
+    echo "To run benchmarks, you need bent: go get github.com/dr2chase/bent; bent -I"
+    exit 1
+fi
+
+if [ ! -e gopath ] ; then
+    echo "To run benchmarks, you need to initialize this directory for running bent (it writes a bunch of files): bent -I"
+    exit 1
+fi
+
+BENCH_INLINES="${BENCH}.inlines"
+BENCH_TRIALS="${BENCH}.trials"
+BENCH_STAT="${BENCH}.laststat"
+BENCH_LOG="${BENCH}.log"
+
+# These ought to be okay.
+THRESHOLD=67
+COUNT=10000
+SEED0=1
+
+# Begin one after the last trial run
+if [ -e "${BENCH_TRIALS}" ] ; then
+    SEED=`tail -1 "${BENCH_TRIALS}" | awk -F , '{print 0+$2}'`
+    SEED0=$((SEED + 1))
+    echo "Resuming at seed=${SEED0}"
+fi
+
+# Create the record of all the inlines if none exists
+if [ ! -e "${BENCH_INLINES}" ] ; then 
+   echo "Creating list of all inline sites in ${BENCH_INLINES}"
+   bent -U -v -b "${BENCH}" -N=0 -C conf-inl.toml -c RECORD  > "${BENCH_INLINES}".tmp
+   grep INLINE_SITE "${BENCH_INLINES}".tmp | sort -u > "${BENCH_INLINES}"
+fi
+
+SEEDN=$((SEED0 + COUNT))
+
+# FYI `eval echo {${SEED0}..${SEEDN}}` does what you want.
+for S in `eval echo {${SEED0}..${SEEDN}}` ; do 
+# expects environment variables T and S to be set
+while \
+        rm -rf goroots testbin
+        solve_inlines -seed ${S} -threshold ${THRESHOLD} "${BENCH_INLINES}" > inlines.txt
+        $PERFLOCK bent -U -v -b "${BENCH}" -N=5 -C conf-inl.toml -c TEST >> "${BENCH_LOG}"
+        benchstat -geomean -csv testbin/*.TEST.stdout >& "${BENCH_STAT}"
+
+        tail -1 "${BENCH_STAT}" >> "${BENCH_LOG}"
+        # Noise depends on whether it is one benchmark or several
+        if grep -q "Geo mean" "${BENCH_STAT}" ; then
+                # Extract max noise from all the benchmarks.
+                # awk: using comma as field separator, match 3-field lines where last field ends in "%", compute maxnoise, print it at the end.
+                NOISE=`awk -F , 'NF == 3 && $3~/.*%/ { gsub(" ","",$3); noise = 0+$3; if (noise > maxnoise) maxnoise=noise;} END {print maxnoise;}' < "${BENCH_STAT}" `
+        else
+                NOISE=`tail -1 "${BENCH_STAT}" | awk -F , '{gsub(" ","",$3); print $3}' | sed -e '1,$s/%//'`
+        fi
+        TIME=`tail -1 "${BENCH_STAT}" | awk -F , '{gsub(" ","",$2); print $2}' ` 
+        echo "Seed=${S}, Threshold=${THRESHOLD}, Time=${TIME}, Max noise=${NOISE}"
+        if test -z "${NOISE}" ; then
+                true
+        else
+                test ${NOISE} -gt ${MAXNOISE}
+        fi
+do
+        echo "Too noisy (${NOISE}), repeating test"
+done 
+
+echo "${THRESHOLD},${S},${TIME},${NOISE}" >> "${BENCH_TRIALS}"
+done
---
+++ solve_inlines.go
@@ -0,0 +1,369 @@
+// Copyright 2019 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"github.com/google/gxui/math"
+	"gonum.org/v1/gonum/mat"
+	"io"
+	"math/rand"
+	"os"
+	"runtime/pprof"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+// inlineRecord describes the relevant parts of a compiler-emitted record of an inlining decision.
+type inlineRecord struct {
+	callerPackage  string
+	callerFunction string
+	callerLine     int32
+	callerColumn   int32
+	inlinePackage  string
+	inlineFunction string
+	inlineSize     int32
+	line           []string // save this for constructed best/worst subsets.
+}
+
+// a benchmarkTrial describes a particular build and benchmark run.
+// seed and threshold determine which inlining sites are activated and the benchmark time that results.
+type benchmarkTrial struct {
+	seed      int64
+	threshold int32
+	time      float64
+	noise     int32 // Non-negative if present.  Not used yet.
+}
+
+// fillRow initializes a row for least squares solution, given
+// a seed and threshold to determine which elements are present.
+// The algorithm here is a duplicate of the one in the experimental
+// inl.go.
+func fillRow(seed int64, threshold int32, row []float64, avg float64) {
+	rng := rand.New(rand.NewSource(seed))
+	row[len(row)-1] = avg // Constant term
+	for i := 0; i < len(row)-1; i++ {
+		if rng.Int31n(100) < threshold {
+			row[i] = 1
+		} else {
+			row[i] = 0
+		}
+	}
+}
+
+// readInlines reads a file of inline information used to generate randomized-inlining benchmark results.
+func readInlines(fileName string) []inlineRecord {
+	f, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	r := csv.NewReader(f)
+	r.Comment = '#'
+	var result []inlineRecord
+	for {
+		line, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		// Add inlining record
+		//                            0                                1         2           3            4          5   6    7              8             9
+		// goroots/TEST/src/internal/reflectlite/value.go:440:25: ,front_end,INLINE_SITE,reflectlite,Value.assignTo,440,25,reflectlite,directlyAssignable,217
+		callerLine, err := strconv.Atoi(line[5])
+		if err != nil {
+			panic(err)
+		}
+		callerColumn, err := strconv.Atoi(line[6])
+		if err != nil {
+			panic(err)
+		}
+		inlineSize, err := strconv.Atoi(line[9])
+		if err != nil {
+			panic(err)
+		}
+		rec := inlineRecord{callerPackage: line[3], callerFunction: line[4], callerLine: int32(callerLine), callerColumn: int32(callerColumn),
+			inlinePackage: line[7], inlineFunction: line[8], inlineSize: int32(inlineSize), line: line}
+		result = append(result, rec)
+	}
+	return result
+}
+
+// readBenchmarks parses a file full of benchmark records.
+func readBenchmarks(fileName string) []benchmarkTrial {
+	f, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	r := csv.NewReader(f)
+	r.Comment = '#'
+	var result []benchmarkTrial
+
+	for {
+		line, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err)
+		}
+		threshold, err := strconv.Atoi(strings.TrimSpace(line[0]))
+		if err != nil {
+			panic(err)
+		}
+		seed, err := strconv.Atoi(strings.TrimSpace(line[1]))
+		if err != nil {
+			panic(err)
+		}
+		time, err := strconv.ParseFloat(strings.TrimSpace(line[2]), 64)
+		if err != nil {
+			panic(err)
+		}
+		noise := -1
+		if len(line) >= 4 {
+			noise, err = strconv.Atoi(strings.TrimSpace(line[3]))
+			if err != nil {
+				panic(err)
+			}
+		}
+		rec := benchmarkTrial{seed: int64(seed), threshold: int32(threshold), time: time, noise:int32(noise)}
+		result = append(result, rec)
+	}
+	return result
+}
+
+func main() {
+	bestN := 0
+	worstN := 0
+	seed := 0
+	threshold := 67
+	flag.IntVar(&bestN, "best", bestN, "print the best N inlines in CSV form (best first)")
+	flag.IntVar(&worstN, "worst", worstN, "print the worst N inlines in CSV form (worst last)")
+	flag.IntVar(&seed, "seed", seed, "Seed for generating a random selection of a file")
+	flag.IntVar(&threshold, "threshold", threshold, "Percentage (1-100) of randomly selected lines to include")
+	var cpuProfile = flag.String("cpuProfile", "", "write cpu profile to file")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr,
+			`
+%s [-best N] [-worst N] inlinesFile randomBenchFile  
+or
+%s -seed N [-threshold N] inlinesFile > inlinesSubsetFile
+
+The first form combines an inlines file with a file containing lines summarizing randomized choice benchmark runs,
+where each line has the form "threshold,seed,result" and threshold is a number between 0 and 100 inclusive, to allow
+a least-squares estimation of which inlining sites are most important.  -best and -worst modify the output to
+be ordered lists of the best (best-first) and worst (worst-last) lines from the point-of-view of improving benchmark
+performance.
+
+The second form uses seed and threshold (default 67) to generate an input for a single randomized choice benchmark run.
+The subset file is written to standard out.
+
+Because solving for the best/worst inlines can be time-consuming, %s also supports the "-cpuProfile file" option.
+`, os.Args[0], os.Args[0])
+	}
+
+	flag.Parse()
+	args := flag.Args()
+
+	randomBenchFile := ""
+
+	if seed == 0 {
+		if len(args) != 2 {
+			fmt.Println("two files are required.")
+			flag.Usage()
+			return
+		}
+		randomBenchFile = args[1]
+	} else if bestN > 0 || worstN > 0 || len(args) != 1 {
+		fmt.Println("-seed is incompatible with -bestN, -worstN, and requires a single file argument")
+		flag.Usage()
+		return
+	}
+	inlinesFile := args[0]
+
+	if *cpuProfile != "" {
+		f, err := os.Create(*cpuProfile)
+		if err != nil {
+			panic(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	inlines := readInlines(inlinesFile)
+
+	if seed > 0 { // Produce subset, exit.
+		vstore := make([]float64, 1+len(inlines))
+		// Use fillRow because thus only one copy of the RNG selection.
+		fillRow(int64(seed), int32(threshold), vstore, -1)
+		for i, l := range inlines {
+			if vstore[i] > 0 {
+				value := ""
+				for j, s := range l.line {
+					a := s
+					if strings.Contains(s, ",") || len(s) > 0 && s[0] == '"' {
+						// Quoting is required for CSV.
+						if strings.ContainsAny(s, "\"") {
+							s = strings.Replace(s, "\"", "\"\"", -1)
+						}
+						a = "\"" + s + "\""
+					}
+					if j > 0 {
+						value += ","
+					}
+					value += fmt.Sprintf("%v", a)
+				}
+				fmt.Println(value)
+			}
+		}
+		return
+	}
+
+	trials := readBenchmarks(randomBenchFile)
+
+	// Get max, min, and average benchmark times.
+	max := 0.0
+	min := 1.0e100
+	total := 0.0
+	for _, t := range trials {
+		if t.time > max {
+			max = t.time
+		}
+		if t.time < min {
+			min = t.time
+		}
+		total += t.time
+	}
+	avg := total / float64(len(trials))
+
+	a := mat.NewDense(len(trials), 1+len(inlines), nil)
+	vstore := make([]float64, 1+len(inlines))
+	v := mat.NewVecDense(1+len(inlines), vstore)
+	b := mat.NewVecDense(len(trials), nil)
+	for i, t := range trials {
+		fillRow(t.seed, t.threshold, a.RawRowView(i), avg)
+		b.SetVec(i, float64(t.time))
+	}
+	fmt.Printf("# Number of inlines is %d, trials is %d, min time is %f, avg time is %f, max time is %f\n", len(inlines), len(trials), min, avg, max)
+
+	v.SolveVec(a, b)
+	// vstats := append([]float64{}, vstore...)
+	sortedOrder := make([]int, len(vstore))
+	for i := range sortedOrder {
+		sortedOrder[i] = i
+	}
+	sort.Slice(sortedOrder, func(i, j int) bool {
+		return vstore[sortedOrder[i]] < vstore[sortedOrder[j]]
+	})
+
+	if bestN > 0 || worstN > 0 {
+		if bestN > 0 {
+			for i := 0; i < math.Min(bestN, len(sortedOrder)); i++ {
+				line := inlines[sortedOrder[i]].line
+				fmt.Printf("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",
+					line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9])
+			}
+			if worstN > 0 {
+				fmt.Println() // insert a separating blank line.
+			}
+		}
+		if worstN > 0 {
+			// First to check to see if constant term is in worst N.
+			l := math.Min(worstN, len(sortedOrder))
+			for i := 0; i < l; i++ {
+				j := len(sortedOrder) - l + i
+				o := sortedOrder[j]
+				if o == len(inlines) && l < len(sortedOrder) {
+					l++
+					break
+				}
+			}
+			for i := 0; i < l; i++ {
+				j := len(sortedOrder) - l + i
+				o := sortedOrder[j]
+				// Check for o = len(inlines)
+				if o == len(inlines) {
+					continue
+				}
+				line := inlines[o].line
+				fmt.Printf("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",
+					line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9])
+			}
+		}
+		return
+	}
+
+	fmt.Printf("Base term = %f\n", vstore[len(vstore)-1])
+
+	for i := 0; i < 11; i++ {
+		j := (i*len(sortedOrder) + 5) / 10
+		if j == len(sortedOrder) {
+			j--
+		}
+		fmt.Printf("%dth percentile[%d] = %f\n", i*10, j, vstore[sortedOrder[j]])
+	}
+
+	for i := 0; i < math.Min(50, len(sortedOrder)); i++ {
+		rec := inlines[sortedOrder[i]]
+		fmt.Printf("sorted[%d] = %f, %s.%s at %d:%d inlines %s.%s, size %d\n",
+			i, vstore[sortedOrder[i]],
+			rec.callerPackage, rec.callerFunction, rec.callerLine, rec.callerColumn,
+			rec.inlinePackage, rec.inlineFunction, rec.inlineSize)
+	}
+	fmt.Println()
+	for i := math.Min(50, len(sortedOrder)); i > 0; i-- {
+		j := len(sortedOrder) - i
+		k := sortedOrder[j]
+		if k >= len(inlines) {
+			// Constant term
+			fmt.Printf("sorted[%d] = %f, constant term\n", j, vstore[k])
+			continue
+		}
+		rec := inlines[k]
+		fmt.Printf("sorted[%d] = %f, %s.%s at %d:%d inlines %s.%s, size %d\n",
+			j, vstore[k],
+			rec.callerPackage, rec.callerFunction, rec.callerLine, rec.callerColumn,
+			rec.inlinePackage, rec.inlineFunction, rec.inlineSize)
+	}
+
+	count := 0
+	benefit := 0.0
+	printAtBenefit := -1.0
+	for k, i := range sortedOrder {
+		v := vstore[i]
+		if v >= 0 {
+			break
+		}
+		count++
+		benefit += v
+		if benefit <= printAtBenefit {
+			if k < 50 {
+				fmt.Printf("At %d, alleged benefit is %f, last = %f\n", count, benefit, v)
+			}
+			printAtBenefit -= 1
+		}
+	}
+	fmt.Printf("Number of negative coefficients = %d, alleged total benefit = %f\n", count, benefit)
+
+}
